c27be18716a91ea6a8c7a896f6495c76aba2fadf
Brandon <bhanks@mit.edu>
2018-05-02 23:46:25 -0400

    fixed urls

--- datacollection/urls.py
+++ datacollection/urls.py
@@ -3,16 +3,14 @@ from rest_framework import routers
 # from django.conf import settings
 from . import views
 
-
-
 router = routers.DefaultRouter()
 router.register(r'gamesession', views.GameSessionViewSet)
-router.register(r'events', views.EventViewSet)
+router.register(r'event', views.EventViewSet)
 
 
-#
-# urlpatterns = [
-#     url(r'^api/gamesession/?$', views.GameSessionViewSet),
-#     url(r'^api/events/?$', views.EventViewSet),
-#     url(r'^api/', include(router.urls)),
-#     ]
+
+urlpatterns = [
+    # url(r'^api/gamesession/?$', views.GameSessionViewSet),
+    # url(r'^api/event/?$', views.EventViewSet),
+    url(r'^api/', include(router.urls)),
+    ]
