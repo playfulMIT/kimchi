746a22251d133fcc6cdfce952bda0eac34858a9c
Brandon <bhanks@mit.edu>
2018-05-02 23:42:36 -0400

    fixed url

--- datacollection/urls.py
+++ datacollection/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register(r'sessions', views.GameSessionViewSet)
+router.register(r'gamesession', views.GameSessionViewSet)
 router.register(r'events', views.EventViewSet)
 
 
 
 urlpatterns = [
     url(r'^api/gamesession/?$', views.GameSessionViewSet),
-    url(r'^api/event/?$', views.EventViewSet),
+    url(r'^api/events/?$', views.EventViewSet),
     url(r'^api/', include(router.urls)),
     ]
