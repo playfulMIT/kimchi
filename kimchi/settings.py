a0832aad0807925f919bb39fff3e8bf792419b02
Brandon <bhanks@mit.edu>
2018-05-03 00:17:09 -0400

    fixing database

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -77,44 +77,44 @@ WSGI_APPLICATION = 'kimchi.wsgi.application'
 
 # # Database
 # # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-# PAAS settings -------------------------------------------------
-# import django_heroku
-# django_heroku.settings(locals())
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# # SECURE_SSL_REDIRECT=True
-#
-#
-# # Database
-# # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-#
 #
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'kimchi',                      # Or path to database file if using sqlite3.
-#         # The following settings are not used with sqlite3:
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',                      # Set to empty string for default.
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-#
-# # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# dbconfig = dj_database_url.config()
-# if dbconfig:
-#     DATABASES['default'] = dbconfig
+
+
+# PAAS settings -------------------------------------------------
+import django_heroku
+django_heroku.settings(locals())
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT=True
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'kimchi',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+dbconfig = dj_database_url.config()
+if dbconfig:
+    DATABASES['default'] = dbconfig
 
 
 
