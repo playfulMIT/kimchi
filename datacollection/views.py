acb310ddc2431252b565e839608b0624e645f0a0
Brandon <bhanks@mit.edu>
2018-05-16 16:37:50 -0400

    rewrote the create method for gamesessions

--- datacollection/views.py
+++ datacollection/views.py
@@ -3,6 +3,7 @@ from datacollection.utils import get_client_ip
 from .models import GameSession, Event
 from .serializers import GameSessionSerializer, EventSerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
 
 import json
 
@@ -17,22 +18,18 @@ class GameSessionViewSet(viewsets.ModelViewSet):
     serializer_class = GameSessionSerializer
 
     def create(self, request, *args, **kwargs):
-        print(request.data)
-        data = json.loads(request.data)
-        data['session_id'] = request.session
-        request.data = json.dumps(data)
-        print("request data: \n")
-        print(request.data)
-        super.create(self, request, *args, **kwargs)
-
-    def perform_create(self, serializer):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         ip_list = get_client_ip(self.request).split(',')
-        public_ip = str(ip_list[len(ip_list)-1])
+        public_ip = str(ip_list[len(ip_list) - 1])
         other_ip = None
         if len(ip_list) > 1:
             other_ip = str(ip_list[0])
         serializer.save(client_ip=public_ip,
-                        client_ip_other=other_ip)
+                        client_ip_other=other_ip,
+                        session_id=request.session)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class EventViewSet(viewsets.ModelViewSet):
