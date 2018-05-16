d5f2aa6db69d0391a48b57cc44f062271b205a1b
Brandon <bhanks@mit.edu>
2018-05-16 16:16:11 -0400

    oops had attached method to wrong view

--- datacollection/views.py
+++ datacollection/views.py
@@ -16,23 +16,6 @@ class GameSessionViewSet(viewsets.ModelViewSet):
     queryset = GameSession.objects.all()
     serializer_class = GameSessionSerializer
 
-    def perform_create(self, serializer):
-        ip_list = get_client_ip(self.request).split(',')
-        public_ip = str(ip_list[len(ip_list)-1])
-        other_ip = None
-        if len(ip_list) > 1:
-            other_ip = str(ip_list[0])
-        serializer.save(client_ip=public_ip,
-                        client_ip_other=other_ip)
-# @csrf_exempt
-class EventViewSet(viewsets.ModelViewSet):
-
-    """
-    API endpoint that allows fingerprints to be viewed or edited.
-    """
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-
     def create(self, request, *args, **kwargs):
         print(request.data)
         data = json.loads(request.data)
@@ -41,6 +24,25 @@ class EventViewSet(viewsets.ModelViewSet):
         print("request data: \n")
         print(request.data)
         super.create(self, request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        ip_list = get_client_ip(self.request).split(',')
+        public_ip = str(ip_list[len(ip_list)-1])
+        other_ip = None
+        if len(ip_list) > 1:
+            other_ip = str(ip_list[0])
+        serializer.save(client_ip=public_ip,
+                        client_ip_other=other_ip)
+
+
+class EventViewSet(viewsets.ModelViewSet):
+
+    """
+    API endpoint that allows fingerprints to be viewed or edited.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
 
 
 
