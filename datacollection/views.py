ee6bfd804031d67250677fcb92c87c41ff403749
Brandon <bhanks@mit.edu>
2018-07-12 15:12:18 -0400

    more csv updates

--- datacollection/views.py
+++ datacollection/views.py
@@ -91,7 +91,7 @@ class Echo:
         self.header_written = False
     def write(self, value):
         if not self.header_written:
-            value = self.header + '\n' + str(value)
+            value = self.header + '\n' + str(value) + '\n'
             self.header_written = True
         """Write the value by returning it, instead of storing in a buffer."""
         value_string = str(value) + '\n'
