7c33dd3513f7fcce934da5903065781761e8ccf2
Brandon <bhanks@mit.edu>
2018-07-30 23:56:55 -0400

    fixing session error

--- games/views.py
+++ games/views.py
@@ -12,9 +12,9 @@ def mitfp(request):
     # return HttpResponse(request.session.session_key)
 
 def shapes(request):
-    request.session.save()
+    # request.session.save()
     return render(request, 'games/shapes.html', {'title':"shapes 0.2.0 playtest 2",'sessionID':request.session.session_key})
 
 def playtest(request):
-    request.session.save()
+    # request.session.save()
     return render(request, 'games/playtest.html', {'title':"shapes 0.3.0 playtest 3",'sessionID':request.session.session_key})
