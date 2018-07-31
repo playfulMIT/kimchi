fc61d8d993b7d910d11d7ea50bdbaa71300f991b
Brandon <bhanks@mit.edu>
2018-07-30 23:34:52 -0400

    forgot to rename a function

--- games/views.py
+++ games/views.py
@@ -15,6 +15,6 @@ def shapes(request):
     request.session.save()
     return render(request, 'games/shapes.html', {'title':"shapes 0.2.0 playtest 2",'sessionID':request.session.session_key})
 
-def shapes(request):
+def playtest(request):
     request.session.save()
     return render(request, 'games/playtest.html', {'title':"shapes 0.3.0 playtest 3",'sessionID':request.session.session_key})
