37fdb665266c90ce3adfa327c61761f1b12ca6b3
Brandon <bhanks@mit.edu>
2018-09-19 16:03:02 -0400

    more channels integration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn kimchi.wsgi:application
+web: daphne kimchi.asgi:application --port $PORT --bind 0.0.0.0 -v2
---
+++ datacollection/consumers.py
@@ -0,0 +1,18 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+import re
+
+
+
+class DataCollectionConsumer(AsyncWebsocketConsumer):
+
+    async def websocket_connect(self, event):
+        await self.send({
+            "type": "websocket.accept",
+        })
+
+    async def websocket_receive(self, event):
+        await self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })
