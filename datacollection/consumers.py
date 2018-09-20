5f6ba83ed17cad9368ce74ac9ba0b6426b7174f4
Brandon <bhanks@mit.edu>
2018-09-20 00:16:12 -0400

    testing

--- datacollection/consumers.py
+++ datacollection/consumers.py
@@ -1,40 +1,44 @@
-# from channels.generic.websocket import AsyncWebsocketConsumer
-# import json
-# import re
-#
-#
-#
-# class DataCollectionConsumer(AsyncWebsocketConsumer):
-#
-#     async def connect(self):
-#         print("connect")
-#         await self.send({
-#             "type": "websocket.accept",
-#         })
-#
-#         async def receive(self, text_data):
-#         print(event)
-#         await self.send({
-#             "type": "websocket.send",
-#             "text": event["text"],
-#         })
-
-
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+import re
+
+
 
 class DataCollectionConsumer(AsyncWebsocketConsumer):
-    groups = ["broadcast"]
 
     async def connect(self):
         print("connect")
-
+        # await self.send({
+        #     "type": "websocket.accept",
+        # })
         await self.accept()
 
-    async def receive(self, text_data=None, bytes_data=None):
-        print("recieve")
-        await self.close()
-
-
-    async def disconnect(self, close_code):
-        print("disconnect")
-
+        async def receive(self, text_data):
+        print(text_data)
+        print("got data")
+        # await self.send({
+        #     "type": "websocket.send",
+        #     "text": event["text"],
+        # })
+        async def disconnect(self):
+            print("disconnect")
+#
+#
+# from channels.generic.websocket import AsyncWebsocketConsumer
+#
+# class DataCollectionConsumer(AsyncWebsocketConsumer):
+#     groups = ["broadcast"]
+#
+#     async def connect(self):
+#         print("connect")
+#
+#         await self.accept()
+#
+#     async def receive(self, text_data=None, bytes_data=None):
+#         print("recieve")
+#         await self.close()
+#
+#
+#     async def disconnect(self, close_code):
+#         print("disconnect")
+#
