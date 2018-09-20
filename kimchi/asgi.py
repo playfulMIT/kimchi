23b878ab09801119890ca36f7b0b38735afa0089
Brandon <bhanks@mit.edu>
2018-09-19 21:58:53 -0400

    asgi fix

--- kimchi/asgi.py
+++ kimchi/asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elk.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kimchi.settings")
 django.setup()
 application = get_default_application()
