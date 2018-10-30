06e57edec8c98ec5dff5b7ae96d135d41afd55a8
Brandon <bhanks@mit.edu>
2018-10-30 16:19:06 -0400

    url reorder

--- kimchi/urls.py
+++ kimchi/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('datacollection.urls')),
     path('', include('games.urls')),
-    path('admin/', admin.site.urls),
-
 ]
