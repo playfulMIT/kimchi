0de22ee829ba29c20e06c1de905c4bb0d05f6385
Brandon <bhanks@mit.edu>
2018-10-30 16:35:37 -0400

    added models to admin site

--- datacollection/admin.py
+++ datacollection/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Event, URL, Player, PlayerSession
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(URL)
+admin.site.register(Player)
+admin.site.register(PlayerSession)
