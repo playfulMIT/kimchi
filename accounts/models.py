d189534e7f13630fd22c94164a684170116960f4
Brandon <bhanks@mit.edu>
2018-11-14 07:19:32 -0500

    testing user acct

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 class CustomUserManager(UserManager):
-    def get_by_natural_key(self, username):
-        case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
-        return self.get(**{case_insensitive_username_field: username})
+    # def get_by_natural_key(self, username):
+    #     case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
+    #     return self.get(**{case_insensitive_username_field: username})
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
