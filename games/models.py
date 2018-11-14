8cca881a88adea7085a2dd73059f72e4c3c10229
Brandon <bhanks@mit.edu>
2018-11-14 03:38:10 -0500

    added level models

--- games/models.py
+++ games/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from datacollection.models import Player
+# from datacollection.models import Player
 # Create your models here.
 
 class Level(models.Model):
-    data = models.TextField()
-    owner = models.ForeignKey(Player, null=True, on_delete=models.SET_NULL)
+    puzzleName = models.CharField(max_length=50)
+    description = models.TextField()
+    gridDim = models.IntegerField(null=True,blank=True)
+    shapeData = models.TextField()
+    # owner = models.ForeignKey(Player, null=True, on_delete=models.SET_NULL)
+    solutionCameraAngles = models.CharField(max_length=50)
 
+class LevelSet(models.Model):
+    levels = models.ManyToManyField(Level)
+    canPlay = models.BooleanField(default=True)
