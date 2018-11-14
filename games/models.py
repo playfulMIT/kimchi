49033fcb6a1b84e007b5a366e8c8b7407996cf3e
Brandon <bhanks@mit.edu>
2018-11-14 09:09:53 -0500

    making config builder

--- games/models.py
+++ games/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Level(models.Model):
-    puzzleName = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     gridDim = models.IntegerField(null=True,blank=True)
     shapeData = models.TextField()
