610fb62efc266af45ceae8d6a8439729fcab72b7
Brandon <bhanks@mit.edu>
2018-12-14 22:55:36 -0500

    homepage fix

--- games/urls.py
+++ games/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('sept18/', views.gamews),
     path('stg0910/', views.stg0910),
     path('stg0924/', views.stg0924),
-    path('', RedirectView.as_view(url='https://shadowspect.org')),
+    path('', RedirectView.as_view(url='http://shadowspect.org')),
     re_path(r'^(?P<slug>[a-zA-Z0-9-]+)/$', views.wildcard_url),
 ]
 
