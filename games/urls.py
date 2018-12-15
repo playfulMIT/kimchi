d998de91c86d124f4e61d0540820a0bcd79418e5
Brandon <bhanks@mit.edu>
2018-12-14 22:51:34 -0500

    homepage fix

--- games/urls.py
+++ games/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 # from django.conf import settings
 from . import views
 from django.urls import path, re_path
-from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('test/', views.mitfp),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('sept18/', views.gamews),
     path('stg0910/', views.stg0910),
     path('stg0924/', views.stg0924),
-    # path('', redirect('https://shadowspect.org')),
+    path('', RedirectView.as_view(url='https://shadowspect.org')),
     re_path(r'^(?P<slug>[a-zA-Z0-9-]+)/$', views.wildcard_url),
 ]
 
