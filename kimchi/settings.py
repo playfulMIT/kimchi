6a7b35168ca90d69df023e26c7a3dd47d2722bd0
Brandon <bhanks@mit.edu>
2019-01-14 17:47:26 -0500

    favicon

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# WHITENOISE_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'rootfiles')
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'rootfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
