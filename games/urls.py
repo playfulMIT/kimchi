09cfedaaacdff9a4e253cd82c3763d0da6c40187
Brandon <bhanks@mit.edu>
2019-02-11 13:55:58 -0500

    mturk stubs

--- games/urls.py
+++ games/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('mturk/', views.mturk),
     path('', RedirectView.as_view(url='http://shadowspect.org')),
     re_path(r'^(?P<slug>[a-zA-Z0-9-]+)/$', views.wildcard_url),
 ]
--- games/views.py
+++ games/views.py
@@ -12,3 +12,11 @@ def wildcard_url(request, slug):
     request.session['urlpk'] = url.pk
     return render(request, 'games/test.html',
                   {'title': "Shadow Tangrams", 'sessionID': request.session.session_key})
+
+def mturk(request, slug):
+    if not request.session.session_key:
+        request.session.save()
+    url = get_object_or_404(URL, pk=slug)
+    request.session['urlpk'] = url.pk
+    return render(request, 'games/mturk.html',
+                  {'title': "Shadow Tangrams", 'sessionID': request.session.session_key})
