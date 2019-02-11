6adfbd787b6a92ee00acfb5a0353a19398a7dfb7
Brandon <bhanks@mit.edu>
2019-02-11 14:07:06 -0500

    mturk

--- games/views.py
+++ games/views.py
@@ -13,10 +13,8 @@ def wildcard_url(request, slug):
     return render(request, 'games/test.html',
                   {'title': "Shadow Tangrams", 'sessionID': request.session.session_key})
 
-def mturk(request, slug):
+def mturk(request):
     if not request.session.session_key:
         request.session.save()
-    url = get_object_or_404(URL, pk=slug)
-    request.session['urlpk'] = url.pk
     return render(request, 'games/mturk.html',
                   {'title': "Shadow Tangrams", 'sessionID': request.session.session_key})
