00b218e593e6d0d13e37d79a97d1f0e66b72fcb6
Brandon <bhanks@mit.edu>
2019-02-15 15:33:38 -0500

    more session testing

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -76,7 +76,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Ensures sessions persist
 # https://docs.djangoproject.com/en/2.0/topics/http/sessions/#configuring-sessions
-SESSION_ENGINE = "datacollection.models.SessionStore"
+SESSION_ENGINE = "datacollection.models"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # SESSION_SAVE_EVERY_REQUEST = True
 SESSION_COOKIE_HTTPONLY = False
