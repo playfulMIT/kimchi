52458ff133301416aaab5fd9a07b79065e97db0a
Brandon <bhanks@mit.edu>
2019-02-15 16:13:21 -0500

    added hardware profile capture to view

--- shadowspect/views.py
+++ shadowspect/views.py
@@ -17,7 +17,8 @@ def mturk(request):
     if not request.session.session_key:
         request.session.save()
     print('metadata:')
-    print(request.META.get('HTTP_USER_AGENT'))
-    print(request.META.get('REMOTE_ADDR'))
+    request.session.useragent = request.META.get('HTTP_USER_AGENT')
+    request.session.ip = request.META.get('REMOTE_ADDR')
+    request.session.save()
     return render(request, 'shadowspect/mturk.html',
                   {'title': "Shadow Tangrams", 'sessionID': request.session.session_key})
