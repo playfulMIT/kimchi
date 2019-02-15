fd23fce50e562bc0d849380d2a0f66ee50084c27
Brandon <bhanks@mit.edu>
2019-02-14 19:45:19 -0500

    removed session middleware

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
