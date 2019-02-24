43564abad9941bfb1d17ee916e08e603c7f42b4c
Brandon <bhanks@mit.edu>
2019-02-24 15:59:56 -0500

    debug page

--- shadowspect/views.py
+++ shadowspect/views.py
@@ -37,4 +37,18 @@ def mturk(request):
 
 
 def debug(request):
+    if not request.session.session_key:
+        request.session.save()
+    print("session key: " + request.session.session_key)
+    print("session dict: " + str(request.session.model.__dict__))
+    session = CustomSession.objects.defer(None).get(session_key=request.session.session_key)
+    if session.useragent is None:
+        print("assigning useragent: " + str(request.META.get('HTTP_USER_AGENT')))
+        session.useragent = str(request.META.get('HTTP_USER_AGENT'))
+    if session.ip is None:
+        # print("assigning ip: " + str(request.META.get('REMOTE_ADDR')))
+        address = str(request.META.get('REMOTE_ADDR'))
+        request.session['ip'] = address
+    # request.session.modified = True
+    session.save()
     return HttpResponse("debug")
