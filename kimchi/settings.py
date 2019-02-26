d2d0c061469d6c17225b5ef30c606bf1390886de
Brandon <bhanks@mit.edu>
2019-02-25 22:51:53 -0500

    session testing

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -26,7 +26,7 @@ env = environ.Env()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
