e0be1b55a4c1444fd54c8b364c75091f845aeb9c
Brandon <bhanks@mit.edu>
2019-02-25 22:59:55 -0500

    session testing

--- datacollection/models.py
+++ datacollection/models.py
@@ -28,7 +28,7 @@ class SessionStore(DBStore):
         curframe = inspect.currentframe()
         calframe = inspect.getouterframes(curframe, 2)
         print('caller name:', calframe[1][3])
-        print('save details:' + str(self))
+        print('save details:' + str(self.__dict__))
         super().save(must_create)
     # def create_model_instance(self, data):
     #     obj = super().create_model_instance(data)
