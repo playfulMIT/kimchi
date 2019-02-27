d6a31b432bffee062bd5ab10f16d9355fca2bf59
Brandon <bhanks@mit.edu>
2019-02-27 17:21:52 -0500

    moved session generation out of view

--- shadowspect/utils.py
+++ shadowspect/utils.py
@@ -38,22 +38,22 @@ def get_level_json(request, slug):
     return HttpResponse(json.dumps(data))
 
 
-# def generate_session(request):
-#     if not request.session.session_key:
-#         request.session.save()
-#         request.session.accessed = False
-#         request.session.modified = False
-#         print('created session key')
-#     # print("session key: " + request.session.session_key)
-#     # session = CustomSession.objects.get(session_key=request.session.session_key)
-#     #
-#     # if session.useragent is None:
-#     #     session.useragent = str(request.META.get('HTTP_USER_AGENT'))
-#     # if session.ip is None:
-#     #     session.ip = str(request.META.get('REMOTE_ADDR'))
-#     # session.save(update_fields=['useragent', 'ip'])
-#     # session.accessed = False
-#     # session.modified = False
-#     # request.session.accessed = False
-#     # request.session.modified = False
-#     return session
+def generate_session(request):
+    if not request.session.session_key:
+        request.session.save()
+        # request.session.accessed = False
+        # request.session.modified = False
+        print('created session key')
+    # print("session key: " + request.session.session_key)
+    session = CustomSession.objects.get(session_key=request.session.session_key)
+    #
+    # if session.useragent is None:
+    #     session.useragent = str(request.META.get('HTTP_USER_AGENT'))
+    # if session.ip is None:
+    #     session.ip = str(request.META.get('REMOTE_ADDR'))
+    # session.save(update_fields=['useragent', 'ip'])
+    # session.accessed = False
+    # session.modified = False
+    # request.session.accessed = False
+    # request.session.modified = False
+    return session
