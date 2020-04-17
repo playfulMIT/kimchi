2ce7bf6363aae817f91c81279303769388660f2b
Brandon Hanks <bhanks@mit.edu>
2020-04-17 00:00:03 -0400

    added sequence of actions

--- dataprocessing/tasks.py
+++ dataprocessing/tasks.py
@@ -101,8 +101,70 @@ def computeFunnelByPuzzle(group='all'):
     return userFunnelByPuzzle.to_json()
 
 
+def sequenceBetweenPuzzles(group='all'):
+    if group == 'all':
+        toFilter = all_data_collection_urls
+    else:
+        toFilter = group
+
+    urls = URL.objects.filter(name__in=toFilter)
+    sessions = CustomSession.objects.filter(url__in=urls)
+    qs = Event.objects.filter(session__in=sessions)
+    dataEvents = read_frame(qs)
+
+    dataEvents['group'] = [json.loads(x)['group'] if 'group' in json.loads(x).keys() else '' for x in
+                           dataEvents['data']]
+    dataEvents['user'] = [json.loads(x)['user'] if 'user' in json.loads(x).keys() else '' for x in dataEvents['data']]
+    # removing those rows where we dont have a group and a user that is not guest
+    dataEvents = dataEvents[
+        ((dataEvents['group'] != '') & (dataEvents['user'] != '') & (dataEvents['user'] != 'guest'))]
+    dataEvents['group_user_id'] = dataEvents['group'] + '~' + dataEvents['user']
+
+    # Data Cleaning
+    dataEvents['time'] = pd.to_datetime(dataEvents['time'])
+    dataEvents = dataEvents.sort_values('time')
+
+    userPuzzleDict = {}
+
+    for user in dataEvents['group_user_id'].unique():
+        # Select rows
+        user_events = dataEvents[dataEvents['group_user_id'] == user]
+        user_events_na_dropped = user_events.dropna()
+        for enum, event in user_events_na_dropped.iterrows():
+            user_key = event['user']
+            if (user_key not in userPuzzleDict.keys()):
+                userPuzzleDict[user_key] = {}
+                numPuzzles = 1
+            if (event['type'] == 'ws-start_level'):
+                # print('\\start level\\')
+                # print(json.loads(event['data']))
+                activePuzzle = json.loads(event['data'])['task_id']
+                secondKey = str(numPuzzles) + '~' + event['session']
+                userPuzzleDict[user_key][secondKey] = {activePuzzle: ''}
+            elif (event['type'] == 'ws-puzzle_started'):
+                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'started'}
+            elif (event['type'] == 'ws-create_shape'):
+                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'shape_created'}
+            elif (event['type'] == 'ws-check_solution'):
+                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'submitted'}
+            elif (event['type'] == 'ws-puzzle_complete'):
+                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'completed'}
+            elif (event['type'] in ['ws-puzzle_complete', 'ws-exit_to_menu', 'ws-disconnect']):
+                numPuzzles += 1
+
+    userSessionList = []
+    for key in userPuzzleDict.keys():
+        for sequence in userPuzzleDict[key].keys():
+            key_split = sequence.split('~')
+            userSessionList.append([key, key_split[1], key_split[0], userPuzzleDict[key][sequence]])
+
+    userSequence = pd.DataFrame(userSessionList, columns=['user', 'session', 'sequence', 'task_id'])
+
+    return userSequence.to_json()
+
+
 @app.task
-def run_computeFunnelByPuzzle():
+def generate_metadata_and_run_tasks():
     urls = URL.objects.all()
     all_data_collection_urls = []
     interesting_urls = ['ginnymason', 'chadsalyer', 'kristinknowlton', 'lori day', 'leja', 'leja2', 'debbiepoull',
@@ -112,14 +174,31 @@ def run_computeFunnelByPuzzle():
         task = process_task(computeFunnelByPuzzle, [url.name])
         task.input_urls.add(url)
         task.save()
-        
+        task = process_task(sequenceBetweenPuzzles, [url.name])
+        task.input_urls.add(url)
+        task.save()
+
+
     interesting_task = process_task(computeFunnelByPuzzle, interesting_urls)
-    interesting_task.input_urls.add(url)
+    for url in interesting_urls:
+        interesting_task.input_urls.add(URL.objects.get(name=url))
+    interesting_task.save()
+    interesting_task = process_task(sequenceBetweenPuzzles, interesting_urls)
+    for url in interesting_urls:
+        interesting_task.input_urls.add(URL.objects.get(name=url))
     interesting_task.save()
 
     all_task = process_task(computeFunnelByPuzzle, [all_data_collection_urls])
-    all_task.input_urls.add(url)
+    for url in urls:
+        all_task.input_urls.add(url)
     all_task.save()
+    all_task = process_task(sequenceBetweenPuzzles, [all_data_collection_urls])
+    for url in urls:
+        all_task.input_urls.add(url)
+    all_task.save()
+
+
+
 
 
 # @app.on_after_configure.connect
