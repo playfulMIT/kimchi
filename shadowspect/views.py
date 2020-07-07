f61e792b65b1926906a4ce54887d0c046299a27f
Brandon Hanks <bhanks@mit.edu>
2020-07-07 11:08:21 -0400

    changed name ref to id

--- shadowspect/views.py
+++ shadowspect/views.py
@@ -32,13 +32,13 @@ def wildcard_players(request, slug):
     list = {}
     for player in players:
         for session in player.customsession_set.all():
-            list[session.session_key] = player.name.replace(" ", "_")
+            list[session.session_key] = player.id.replace(" ", "_")
     return render(request, "shadowspect/list.html", {"items": list})
 
 
 def wildcard_levels(request, slug, player):
     url_obj = get_object_or_404(URL, pk=slug)
-    selected_player = Player.objects.filter(url=url_obj).get(name=player.replace("_", " "))
+    selected_player = Player.objects.filter(url=url_obj).get(id=player.replace("_", " "))
     print(selected_player)
     levels = {}
     for level in selected_player.attempted.all():
