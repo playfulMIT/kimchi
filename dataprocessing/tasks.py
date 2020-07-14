63f2be38925c2c372f5f761bee4c2bb87c29ca2f
Candace Okumko <cokumko@mit.edu>
2020-07-14 01:49:08 -0400

    debugging

--- dataprocessing/tasks.py
+++ dataprocessing/tasks.py
@@ -589,10 +589,11 @@ def computeLevelsOfActivity(group='all'):
 
     ### MERGING ROWS CORRESPONDING TO THE SAME USER
     activity_dict = activity_by_user.to_dict()
+    raise Exception(activity_dict)
     max_index = len(activity_dict['group'])
     merged_activity = {}
 
-    for i_num in range(1, max_index+1):
+    for i_num in range(max_index):
         i = str(i_num)
 
         if activity_dict['task_id'][i] not in merged_activity:
