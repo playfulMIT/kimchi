4143ed16e5a5e859727f6085f1362ae2bec7fc8a
Candace Okumko <cokumko@mit.edu>
2020-07-27 19:10:31 -0400

    updated sequence between puzzles task

--- dataprocessing/tasks.py
+++ dataprocessing/tasks.py
@@ -105,65 +105,92 @@ def computeFunnelByPuzzle(group='all'):
 
 @app.task
 def sequenceBetweenPuzzles(group='all'):
-    if group == 'all':
+    if group == 'all' : 
         toFilter = all_data_collection_urls
     else:
         toFilter = group
-
+        
     urls = URL.objects.filter(name__in=toFilter)
     sessions = CustomSession.objects.filter(url__in=urls)
     qs = Event.objects.filter(session__in=sessions)
     dataEvents = read_frame(qs)
-
-    dataEvents['group'] = [json.loads(x)['group'] if 'group' in json.loads(x).keys() else '' for x in
-                           dataEvents['data']]
+    
+    dataEvents['group'] = [json.loads(x)['group'] if 'group' in json.loads(x).keys() else '' for x in dataEvents['data']]
     dataEvents['user'] = [json.loads(x)['user'] if 'user' in json.loads(x).keys() else '' for x in dataEvents['data']]
     # removing those rows where we dont have a group and a user that is not guest
-    dataEvents = dataEvents[
-        ((dataEvents['group'] != '') & (dataEvents['user'] != '') & (dataEvents['user'] != 'guest'))]
+    dataEvents = dataEvents[((dataEvents['group'] != '') & (dataEvents['user'] != '') & (dataEvents['user'] != 'guest'))]
     dataEvents['group_user_id'] = dataEvents['group'] + '~' + dataEvents['user']
 
-    # Data Cleaning
+    # filtering to only take the group passed as argument
+    if(group != 'all'):
+        dataEvents = dataEvents[dataEvents['group'].isin(group)]
+    
+       # Data Cleaning
     dataEvents['time'] = pd.to_datetime(dataEvents['time'])
     dataEvents = dataEvents.sort_values('time')
 
     userPuzzleDict = {}
 
     for user in dataEvents['group_user_id'].unique():
-        # Select rows
-        user_events = dataEvents[dataEvents['group_user_id'] == user]
-        user_events_na_dropped = user_events.dropna()
-        for enum, event in user_events_na_dropped.iterrows():
-            user_key = event['user']
-            if (user_key not in userPuzzleDict.keys()):
-                userPuzzleDict[user_key] = {}
-                numPuzzles = 1
-            if (event['type'] == 'ws-start_level'):
-                # print('\\start level\\')
-                # print(json.loads(event['data']))
-                activePuzzle = json.loads(event['data'])['task_id']
-                secondKey = str(numPuzzles) + '~' + event['session']
-                userPuzzleDict[user_key][secondKey] = {activePuzzle: ''}
-            elif (event['type'] == 'ws-puzzle_started'):
-                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'started'}
-            elif (event['type'] == 'ws-create_shape'):
-                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'shape_created'}
-            elif (event['type'] == 'ws-check_solution'):
-                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'submitted'}
-            elif (event['type'] == 'ws-puzzle_complete'):
-                userPuzzleDict[user_key][secondKey] = {activePuzzle: 'completed'}
-            elif (event['type'] in ['ws-puzzle_complete', 'ws-exit_to_menu', 'ws-disconnect']):
-                numPuzzles += 1
-
+            #Select rows
+            user_events = dataEvents[dataEvents['group_user_id'] == user]
+            user_events_na_dropped = user_events.dropna()
+            for enum, event in user_events_na_dropped.iterrows():
+                user_key = event['user']
+                if(user_key not in userPuzzleDict.keys()):
+                    userPuzzleDict[user_key] = {}
+                    numPuzzles = 1
+                if(event['type'] == 'ws-start_level'):
+                    activePuzzle = json.loads(event['data'])['task_id']
+                    if (activePuzzle == 'Sandbox'):
+                        continue
+                    secondKey = str(numPuzzles) + '~' + event['session']
+                    if (userPuzzleDict[user_key].get(secondKey) == None):
+                        userPuzzleDict[user_key][secondKey] = dict()
+                    if (userPuzzleDict[user_key][secondKey].get(activePuzzle) == None):
+                        userPuzzleDict[user_key][secondKey] = {activePuzzle : 'started'}
+                elif(event['type'] == 'ws-puzzle_started'):
+                    if (activePuzzle == 'Sandbox' or userPuzzleDict[user_key][secondKey][activePuzzle] in ['started','shape_created', 'submitted', 'completed']):
+                        continue
+                    userPuzzleDict[user_key][secondKey] = {activePuzzle : 'started'}
+                elif(event['type'] == 'ws-create_shape'):
+                    if (activePuzzle == 'Sandbox' or userPuzzleDict[user_key][secondKey][activePuzzle] in ['shape_created', 'submitted', 'completed']):
+                        continue
+                    userPuzzleDict[user_key][secondKey] = {activePuzzle : 'shape_created'}
+                elif(event['type'] == 'ws-check_solution'):
+                    if (activePuzzle == 'Sandbox' or userPuzzleDict[user_key][secondKey][activePuzzle] in ['submitted', 'completed']):
+                        continue
+                    userPuzzleDict[user_key][secondKey] = {activePuzzle :'submitted'}
+                elif(event['type'] == 'ws-puzzle_complete'):
+                    if (activePuzzle == 'Sandbox'):
+                        continue
+                    userPuzzleDict[user_key][secondKey] = {activePuzzle :'completed'}
+                elif(event['type'] in ['ws-exit_to_menu', 'ws-disconnect', 'ws-login_user']):
+                    if (activePuzzle == 'Sandbox'):
+                        continue
+                    numPuzzles +=1
+                    
     userSessionList = []
     for key in userPuzzleDict.keys():
         for sequence in userPuzzleDict[key].keys():
-            key_split = sequence.split('~')
-            userSessionList.append([key, key_split[1], key_split[0], userPuzzleDict[key][sequence]])
+                key_split = sequence.split('~')
+                userSessionList.append([key, key_split[1], int(key_split[0]), userPuzzleDict[key][sequence]])
 
     userSequence = pd.DataFrame(userSessionList, columns=['user', 'session', 'sequence', 'task_id'])
-
-    return userSequence.to_json()
+    #Recalculate sequence
+    mod = []
+    for user in userSequence['user'].unique():
+            previousAttempt = 1
+            n_attempt = 1
+            individualDf = userSequence[userSequence['user'] == user]
+            for enum, event in individualDf.iterrows():
+                if (event['sequence'] != previousAttempt):
+                    n_attempt += 1
+                previousAttempt = event['sequence']
+                event['sequence'] = n_attempt
+                mod.append(event)
+    modDf = pd.DataFrame(mod, columns=['user', 'session', 'sequence', 'task_id'])
+    return modDf.to_json()
 
 @app.task
 def sequenceWithinPuzzles(group='all'):
@@ -720,16 +747,17 @@ def computeLevelsOfActivityOutliers(group='all'):
     outlier_data = json.loads(task[0])["Pyramids are Strange"]["minmax_normalization"]["all_stats"]
 
     np.random.seed(0)
+    outlier_metrics = list(outlier_data.values())
     n = len(outlier_data.keys())
-    d = len(list(list(outlier_data.values())[0].keys()))
+    d = len(list(outlier_metrics[0].keys()))
 
     # Generate data
     X = np.zeros((n, d))
     for i in range(n):
-        X[i,...] = np.array(list(list(outlier_data.values())[i].values()))
+        X[i,...] = np.array(list(outlier_metrics[i].values()))
 
     num_trees = 100
-    tree_size = 256
+    tree_size = n // 4 
     sample_size_range = (n // tree_size, tree_size)
 
     # Construct forest
@@ -753,8 +781,7 @@ def computeLevelsOfActivityOutliers(group='all'):
         np.add.at(index, codisp.index.values, 1)
     avg_codisp /= index
 
-    return str(avg_codisp)
-
+    return avg_codisp.to_json()
 
 @app.task
 def computeLevelsOfActivity(group='all'):
