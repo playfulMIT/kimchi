56b646c54fcc8c9822cfbe2c73e6737a086f2c69
Brandon Hanks <bhanks@mit.edu>
2020-09-14 10:50:11 -0400

    added replay tool static instance and urls

--- shadowspect/urls.py
+++ shadowspect/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
         r"^static/shadowspect_static/StreamingAssets/(?P<slug>[a-zA-Z0-9-_]+).json",
         get_level_json,
     ),
+    path("static/shadowspect_replay_static/StreamingAssets/config.json", get_config_json),
+    path("static/shadowspect_replay_static/StreamingAssets/generated_replay.json", get_replay_json),
+    re_path(
+        r"^static/shadowspect_replay_static/StreamingAssets/(?P<slug>[a-zA-Z0-9-_]+).json",
+        get_level_json,
+    ),
     # re_path(r"^replay/(?P<slug>[a-zA-Z0-9-]+).json", generate_replay),
     path("mturk/", views.mturk),
     path("levelloader/", views.levelloader),
